6457dc18e50ced8cf0015f3725175d4d87df222f
ErolKocoglu <erolkocoglu44@gmail.com>
2024-01-03 00:09:41 +0300

    Connection Fixes

--- database.py
+++ database.py
@@ -209,7 +209,7 @@ class Database:
 
     def get_games_of_competition(self, competition_id_in):
         games = []
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT 
@@ -240,7 +240,7 @@ class Database:
         return games
 
     def add_game(self, game_in):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = "INSERT INTO games (game_id, competition_id, season, date, home_club_id, away_club_id, home_club_goals, away_club_goals, home_club_position, away_club_position, home_club_manager_name, away_club_manager_name, stadium, attendance, referee, url, home_club_name, away_club_name) VALUES (%s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s) RETURNING game_id;"
                 cursor.execute(query,(game_in.game_id,game_in.competition_id,game_in.season,game_in.date, game_in.home_club_id, game_in.away_club_id, game_in.home_club_goals, game_in.away_club_goals, game_in.home_club_position, game_in.away_club_position,game_in.home_club_manager_name, game_in.away_club_manager_name, game_in.stadium, game_in.attendance, game_in.referee, game_in.url, game_in.home_club_name, game_in.away_club_name))
@@ -248,20 +248,20 @@ class Database:
         return game_key
 
     def update_game(self,game_id, game_in,):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = "UPDATE games SET competition_id = %s, season = %s, date = %s, home_club_id = %s, away_club_id = %s, home_club_goals = %s, away_club_goals = %s, home_club_position =%s, away_club_position =%s,home_club_manager_name = %s, away_club_manager_name = %s, stadium = %s, attendance =%s, referee =%s, url =%s, home_club_name = %s, away_club_name = %s WHERE game_id =%s;"
                 cursor.execute(query, (game_in.competition_id, game_in.season, game_in.date, game_in.home_club_id, game_in.away_club_id, game_in.home_club_goals, game_in.away_club_goals, game_in.home_club_position, game_in.away_club_position,game_in.home_club_manager_name, game_in.away_club_manager_name, game_in.stadium, game_in.attendance, game_in.referee, game_in.url,game_in.home_club_name, game_in.away_club_name,game_id))
 
     def delete_game(self, game_no):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = "DELETE FROM games WHERE game_id=%s;"
                 cursor.execute(query, (game_no,))
 
     def get_goals_of_game(self, game_id_in):
         goals = []
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = "SELECT goal_id, date, game_id, minute, club_name, player_name, description FROM games WHERE game_id=%s;"
                 cursor.execute(query, (game_id_in,))
@@ -296,7 +296,7 @@ class Database:
 
     def get_players_of_club(self,club_id):
         players = []
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                         SELECT
@@ -319,7 +319,7 @@ class Database:
 
 
     def get_player(self, player_id):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT
@@ -377,7 +377,7 @@ class Database:
                                            competition_id)))
         return players
     def get_player_by_name(self, name):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT
@@ -417,7 +417,7 @@ class Database:
         )
         return player
     def add_player(self, player_data):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     INSERT INTO player (player_id, first_name, last_name, name, current_club_name, current_club_id, competition_id)
@@ -433,7 +433,7 @@ class Database:
                     player_data['competition_id']
                 ))
     def update_player(self, player_id, updated_data):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     UPDATE player
@@ -451,7 +451,7 @@ class Database:
                 ))
 
     def delete_player(self, player_id):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = "DELETE FROM player WHERE player_id = %s;"
                 cursor.execute(query, (player_id,))
@@ -460,7 +460,7 @@ class Database:
     #################### CLUBS ###############################
 
     def get_club(self, club_id):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT
@@ -542,7 +542,7 @@ class Database:
         return clubs
     def get_clubs_by_search(self, search_word):
         clubs = []
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """SELECT * FROM clubs WHERE club_id::text LIKE %s OR club_code LIKE %s OR clubs.name LIKE %s ORDER BY club_id;"""
                 search_word = "%" + search_word + "%"
@@ -553,7 +553,7 @@ class Database:
                     clubs.append((club_id, Club(club_id,club_code, name,domestic_competition_id,total_market_value,squad_size,average_age,foreigners_number, foreigners_percentage, national_team_players, stadium_name, stadium_seats, net_transfer_record, coach_name,last_season, url)))
         return clubs
     def get_club_name(self, club_id):
-            with dbapi2.connect(self.db_url) as connection:
+            with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
                 with connection.cursor() as cursor:
                     query = "SELECT name FROM club WHERE (id = %s);"
                     cursor.execute(query, (club_id,))
@@ -561,7 +561,7 @@ class Database:
             return name
 
     def add_club(self, club_in):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """INSERT INTO clubs (club_id, club_code, name, domestic_competition_id)
                 VALUES (%s, %s, %s, %s) RETURNING club_id"""
@@ -569,7 +569,7 @@ class Database:
                 club_key = cursor.fetchone()[0]
         return club_key
     def update_club(self, club_id, club):
-            with dbapi2.connect(self.db_url) as connection:
+            with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
                 with connection.cursor() as cursor:
                     query = """UPDATE clubs
                     SET name = %s, club_code = %s, total_market_value = %s, squad_size = %s, 
@@ -584,7 +584,7 @@ class Database:
 
     def get_clubs(self):
             clubs = []
-            with dbapi2.connect(self.db_url) as connection:
+            with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
                 with connection.cursor() as cursor:
                     query = """
                         SELECT
@@ -648,7 +648,7 @@ class Database:
 
             return clubs
     def delete_club(self, club_id):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     DELETE FROM clubs WHERE (club_id = %s)
@@ -659,7 +659,7 @@ class Database:
     #######################   PLAYER ATTRIBUTES ########################
 
     def get_player_bio(self, player_id):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT
@@ -700,7 +700,7 @@ class Database:
         )
         return player_bio
     def get_all_player_bios(self):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT
@@ -770,7 +770,7 @@ class Database:
         )
         return player_photo
     def get_all_player_photos(self):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     SELECT
@@ -893,7 +893,7 @@ class Database:
 
                 return player_attributes_list
     def add_player_attributes(self, player_attributes_data):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     INSERT INTO player_attributes (player_id, player_code, sub_position, position, foot, height_in_cm, market_value_in_eur, highest_market_value_in_eur, contract_expiration_date)
@@ -914,7 +914,7 @@ class Database:
         return player_key
 
     def update_player_attributes(self, player_attributes_key, player_attributes):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = """
                     UPDATE player_attributes
@@ -936,7 +936,7 @@ class Database:
                     player_attributes_key
                 ))
     def delete_player_attributes(self, player_attributes_key):
-        with dbapi2.connect(self.db_url) as connection:
+        with dbapi2.connect(host="localhost", user = "postgres", password = "12345678", database = "DatabaseProject") as connection:
             with connection.cursor() as cursor:
                 query = "DELETE FROM player_attributes WHERE id = %s"
                 cursor.execute(query, (player_attributes_key,))
